f4c241cff338b3376e1c536ce2af7369732fc7ea
Bruno Oliveira <68453992+TheBrunno@users.noreply.github.com>
2022-09-10 15:25:59 -0300

    fatorial

---
+++ While/Liste 3 de exercicios/Fatorial.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Fatorial {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int i = n-1;
+		while(i >= 1) {
+			n = n*i;
+			i--;
+		}
+		System.out.println(n);
+	}
+}
