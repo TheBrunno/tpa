f065b0bb2dfe52f77a280e8fb4478c1ae0f7db97
Bruno Oliveira <68453992+TheBrunno@users.noreply.github.com>
2022-09-10 15:38:09 -0300

    sla

--- While/Liste 3 de exercicios/Fatorial.java
+++ While/Liste 3 de exercicios/Fatorial.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class Fatorial {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
+		System.out.print("Digite um numero para ver seu fatorial: ");
 		int n = in.nextInt();
 		int i = n-1;
 		while(i >= 1) {
