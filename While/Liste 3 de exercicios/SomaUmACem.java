0e9116d3660cbb51d9b69b4e91afbeb8346726d1
Bruno Oliveira <68453992+TheBrunno@users.noreply.github.com>
2022-09-10 15:25:30 -0300

    soma um a cem

---
+++ While/Liste 3 de exercicios/SomaUmACem.java
@@ -0,0 +1,10 @@
+public class SomaUmACem {
+	public static void main(String[] args) {
+		int i=100, res=0;
+		while(i>=1) {
+			res+=i;
+			i--;
+		}
+		System.out.println("A soma de todos os numeros no intevalo de 1 a 100 é "+res);
+	}
+}
