a102810ba5bbcb765ec41bef51c36ebfb9bcd849
Bruno Oliveira <68453992+TheBrunno@users.noreply.github.com>
2022-09-10 15:24:48 -0300

    tabuada

---
+++ While/Liste 3 de exercicios/Tabuada.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Tabuada {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Digite um numero para ver sua tabuada: ");
+		int num = in.nextInt(), i=1;
+		while(i<=10) {
+			System.out.println(num+" X "+i+" = "+num*i);
+			i++;
+		}
+	}
+}
