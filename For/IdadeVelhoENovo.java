86cfa96c05a5ded6f920f8b873c9a74d49a673e6
Bruno Oliveira <68453992+TheBrunno@users.noreply.github.com>
2022-10-29 12:55:22 -0300

    more 'for' exercises

---
+++ For/IdadeVelhoENovo.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class IdadeVelhoENovo {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int anoAt, anoNa, idade, vel = 0, nov = 0;
+		System.out.print("Digite o ano atual: ");
+		anoAt = in.nextInt();
+		for(int i = 1;i<=10;i++) {
+			System.out.print("Digite o ano de nascimento do usuário "+i+":");
+			anoNa = in.nextInt();
+			idade = anoAt-anoNa;
+			System.out.println(idade);
+			if(idade < nov) {
+				nov = idade;
+			}
+			if(idade > vel) {
+				vel = idade;
+			}
+		}
+		System.out.println("O usuário mais velho tem "+vel+" anos, e o mais novo tem "+nov+" anos");
+	}
+}
---
+++ For/MultiplosDez.java
@@ -0,0 +1,10 @@
+
+public class MultiplosDez {
+	public static void main(String[] args) {
+		for(int i=1;i<=100;i++) {
+			if(i%10==0) {
+				System.out.println(i+" - Múltiplo de 10");
+			}
+		}
+	}
+}
