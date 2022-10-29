9f44f9b3db92d7decc55ee69fc6af9e2b7da46bd
Bruno Oliveira <68453992+TheBrunno@users.noreply.github.com>
2022-10-29 12:40:52 -0300

    more do while exercises

---
+++ Do While/MenorMaiorIdade.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+
+public class MenorMaiorIdade {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int res = 1, anoAt, anoNa, idade;
+		do {
+			System.out.print("Digite o ano atual: ");
+			anoAt = in.nextInt();
+			System.out.print("Digite o ano de nascimento: ");
+			anoNa = in.nextInt();
+			idade = anoAt-anoNa;
+			System.out.println(idade+" anos");
+			if (idade >= 18) {
+				System.out.print("Maior de idade");
+			}else {
+				System.out.print("Menor de idade");
+			}
+			System.out.println("\nDeseja continuar a execução? (1-para SIM ou 2-para NÃO)");
+			res = in.nextInt();
+		}while(res!=2);
+		System.out.println("---- Fim da execução ----");
+	}
+}
