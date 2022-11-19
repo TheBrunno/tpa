9d26f17326c3af711a860912e61a2be9d742bf9f
Bruno Oliveira <68453992+TheBrunno@users.noreply.github.com>
2022-11-19 17:20:10 -0300

    relação divisores i os bagulho

---
+++ Vetores/exercicios para qm é forte ^_^/RelacaoDivisores.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class RelacaoDivisores {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		final int SIZE = 10;
+		int a[] = new int[SIZE];
+		
+		// input
+		for(int i=0; i<SIZE; i++) {
+			System.out.print("Digite o "+(i+1)+"º numero: ");
+			a[i] = in.nextInt();
+		}
+		
+		// processing and output
+		for(int i=0; i<SIZE; i++) {
+			System.out.print("( "+a[i]+" ) ");
+			for(int c=1; c<=a[i]; c++) {
+				if(a[i]%c==0) {
+					System.out.print(c);
+					if(c != a[i] && c != a[i]-1) {
+						System.out.print(" -> ");
+					}
+				}
+			}
+			System.out.println();
+		}
+		in.close();
+	}
+}
